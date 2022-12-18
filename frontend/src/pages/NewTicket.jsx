283438f9fffd5e9e6b1d1109e00a3cbd16c3bd3a
geramrico <geramoralesrico+code@gmail.com>
2022-12-17 21:53:59 -0600

    added AuthStatus to guard new ticket route

---
+++ frontend/src/pages/NewTicket.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function NewTicket() {
+    return (
+        <>
+            {/*Heading Section*/}
+            <section className="heading">
+                <h1>
+                    New Ticket
+                </h1>
+            </section>
+
+
+            {/*Heading Section*/}
+            <section className="form">
+          
+
+            </section>
+        </>
+    )
+}
+
+export default NewTicket
