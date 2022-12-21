b770489f85e71d14c80fee468b5833f222e62d84
geramrico <geramoralesrico+code@gmail.com>
2022-12-21 14:30:31 -0600

    change token expiration to 30d

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -87,7 +87,7 @@ const getMe = asyncHandler(async (req, res) => {
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: 1800
+        expiresIn: '30d'
     })
 }
 
